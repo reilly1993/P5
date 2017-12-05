cc45f720c90b17920a06679add8b47ea754dfc4f
Unknown <vingborg10@gmail.com>
2017-12-05 13:40:49 +0100

    bleh

--- RFID_Sensor/RFID_Sensor.ino
+++ RFID_Sensor/RFID_Sensor.ino
@@ -11,6 +11,16 @@
 #define RST_PIN 9 //write
 MFRC522 mfrc522(SS_PIN, RST_PIN);   // Create MFRC522 instance
 Servo garageServo;
+
+#define ACTORS 2
+String actorIDs[] = {"05 EE F4 C2", "3D 42 2E 72"};
+String actorNames[] = {"cat", "car_1"};
+String drivewayTrigger = "cat";
+
+
+String drivewayActor = "";
+bool isRaining = false;
+bool isDay = false;
  
 void setup() 
 {
@@ -20,37 +30,66 @@ void setup()
   garageServo.attach(9);
   pinMode(8, OUTPUT);
 }
+
+bool card_present = false;
+
 void loop() 
 {
-  if (mfrc522.PICC_IsNewCardPresent() && mfrc522.PICC_ReadCardSerial()) 
-  {
-    String content= "";
-    for (byte i = 0; i < mfrc522.uid.size; i++) {
-       content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
-       content.concat(String(mfrc522.uid.uidByte[i], HEX));
-    }
-    content.toUpperCase();
-    readDrivewayRFID(content.substring(1));
-    delay(1000);
+  //readRFID(mfrc522, "driveway", &drivewayActor);
+  if(drivewayTrigger == drivewayActor) {
+    toggleGarage(garageServo, true);
   } else {
-    garageServo.write(0);
-    digitalWrite(8, LOW);
+    toggleGarage(garageServo, false);
   }
 
-  doStuff();
+  if (!mfrc522.PICC_ReadCardSerial()) {
+    card_present = false;
+    Serial.println("no card");
+  } else {
+    card_present = true; 
+  }
+  
+  delay(500);
 } 
 
-void readDrivewayRFID(String RFID) {
-  if (RFID == "05 EE F4 C2") {
-      Serial.println("cat on driveway");
-      garageServo.write(255);                  // sets the servo position according to the scaled value
-    } else if (RFID == "3D 42 2E 72") {
-      Serial.println("car_1 on driveway");
-      digitalWrite(8, HIGH);
-    } else {
-      Serial.println("Not recognized");
+void readRFID(MFRC522 sensor, String pos, String* actorContainer) {
+  if (sensor.PICC_IsNewCardPresent()) {
+    if (sensor.PICC_ReadCardSerial()) {
+      *actorContainer = "";
+      String content= "";
+      for (byte i = 0; i < sensor.uid.size; i++) {
+         content.concat(String(sensor.uid.uidByte[i] < 0x10 ? " 0" : " "));
+         content.concat(String(sensor.uid.uidByte[i], HEX));
+      }
+      content.toUpperCase();
+      for(int i = 0; i<ACTORS; i++) {
+        if (content.substring(1) == actorIDs[i]) {
+          Serial.print(actorNames[i]); Serial.print(" on "); Serial.println(pos);
+          *actorContainer = actorNames[i];
+        }
+      }
     }
+  } else {
+    *actorContainer = "";
+  }
 }
+
+void toggleLight(int pin, bool state) {
+  digitalWrite(pin, state);
+  Serial.print("Light set "); Serial.println(state);
+}
+
+void toggleWindow(Servo s, bool opened) {
+  s.write(opened ? 255 : 0);
+  Serial.print("Window set "); Serial.println(opened);
+}
+
+void toggleGarage(Servo s, bool opened) {
+  s.write(opened ? 255 : 0);
+  Serial.print("Garage set "); Serial.println(opened);
+}
+
+
 
 typedef struct Brick {
  bool on = false;
@@ -64,7 +103,6 @@ void readRF(char hub, String definition) {
   String brickDef = "";
   int brickNum;
   int actorNum;
-  //garageBricks = {NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL};
   while(getValue(definition, '|', i).length()>0) {
     brickDef = getValue(definition, '|', i);
     if(brickDef.substring(2,3) == ".") { //actor brick
@@ -83,10 +121,6 @@ void readRF(char hub, String definition) {
   }
 }
 
-void doStuff() {
-  
-}
-
 String getValue(String data, char separator, int index)
 {
   int found = 0;
@@ -103,4 +137,3 @@ String getValue(String data, char separator, int index)
 
   return found>index ? data.substring(strIndex[0], strIndex[1]) : "";
 }
-
